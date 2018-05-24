18a7be5e60197d9913335294f72b256469553925
Dowon <blessjdw@gmail.com>
2018-05-24 22:49:42 +0900

    스프링 부트 초기화

---
+++ src/test/java/com/bora/blackberry/BlackberryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bora.blackberry;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BlackberryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
