705ffb3a651e88ef759709743153f0ce5e88b3d8
Dowon <blessjdw@gmail.com>
2018-05-27 00:52:08 +0900

    Article 컬럼 길이 설정

--- src/main/java/com/bora/blackberry/api/v1/domain/article/entity/Article.java
+++ src/main/java/com/bora/blackberry/api/v1/domain/article/entity/Article.java
@@ -8,31 +8,27 @@ import java.time.LocalDateTime;
 
 @Getter
 @Entity
-@Table(name = "articles")
 public class Article {
 
     @Id
     @GeneratedValue
     private long id;
     private String title;
+    @Column(length = 2000)
     private String body;
 
-    @Column(name = "board_id")
     private long boardId;
-    @Column(name = "hits_count")
     private long hitsCount;
-    @Column(name = "likes_count")
     private long likesCount;
-    @Column(name = "dislike_count")
     private long dislikeCount;
 
+    @Column(length = 1)
     @Enumerated(value = EnumType.STRING)
     private IsType deleted;
+    @Column(length = 1)
     @Enumerated(value = EnumType.STRING)
     private IsType reported;
 
-    @Column(name = "created_at")
     private LocalDateTime createdAt;
-    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 }
