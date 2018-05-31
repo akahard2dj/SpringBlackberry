e36b8b516f0609d3e6013d2861660a360f6b5828
Dowon <blessjdw@gmail.com>
2018-05-31 23:17:44 +0900

    글 수정 / 추가 시 글자 길이 벨리데이션 추가

--- src/main/java/com/bora/blackberry/api/v1/article/form/ArticleForm.java
+++ src/main/java/com/bora/blackberry/api/v1/article/form/ArticleForm.java
@@ -4,13 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
 public class ArticleForm {
 
     @NotEmpty
+    @Size(min = 1, max = 30)
     private String title;
+
     @NotEmpty
+    @Size(min = 10, max = 1000)
     private String body;
 }
